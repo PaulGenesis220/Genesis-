d83c786f25a21f14d069a8cc29fc568c57beb1e2
PaulGenesis220 <paulmetzger2004@yahoo.com>
2025-06-06 14:18:46 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-// Genesis relay core logic
-console.log('Genesis started');
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Genesis started'));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Genesis is listening on port ${PORT}`);
+});
