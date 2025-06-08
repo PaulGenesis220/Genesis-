5a9f2f401bae5949159332c01edc218952e99e10
PaulGenesis220 <paulmetzger2004@yahoo.com>
2025-06-08 00:29:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const PORT = 8080;
-
-app.get('/', (req, res) => res.send('Genesis is live'));
-
 app.get('/env-check', (req, res) => {
-  const keyStatus = process.env.CLAUDE_API_KEY
-    ? 'CLAUDE_API_KEY is set'
-    : 'CLAUDE_API_KEY is NOT set';
-  res.send(keyStatus);
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Genesis is listening on port ${PORT}`);
+  const key = process.env.CLAUDE_API_KEY;
+  if (key && key.startsWith("sk-")) {
+    res.send('CLAUDE_API_KEY is set');
+  } else {
+    res.send('CLAUDE_API_KEY is NOT set');
+  }
 });
