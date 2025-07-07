e0645a952fae35d90833cdb922f16549b7100bb7
PaulGenesis220 <paulmetzger2004@yahoo.com>
2025-07-07 16:06:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => res.send('Genesis is live'));
-
-app.get('/env-check', (req, res) => {
-  const keyStatus = process.env.CLAUDE_API_KEY
-    ? 'CLAUDE_API_KEY is set'
-    : 'CLAUDE_API_KEY is NOT set';
-  res.send(keyStatus);
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Genesis is listening on port ${PORT}`);
-});
+// Genesis Safe Mode
+console.log("Genesis container is in passive mode. No code executed.");
+module.exports = {};
